1daae838f40719cecf22e88b11e8c19513115a6b
Daniel Gonzalez <219147853+Taloc-Dev@users.noreply.github.com>
2026-02-27 11:25:11 +0100

    feat: Add an operator controller function

--- src/calculator.js
+++ src/calculator.js
@@ -1,3 +1,7 @@
+let operand1 = 0;
+let operand2 = 0;
+let operator = "+";
+
 function add(a, b){
     return a + b;
 }
@@ -9,4 +13,22 @@ function multiply(a, b){
 }
 function divide(a, b){
     return a / b;
+}
+function operate(){
+    switch(operator){
+        case "+":
+            add(operand1, operand2);
+            break;
+        case "-":
+            substract(operand1, operand2);
+            break;
+        case "*":
+            multiply(operand1, operand2);
+            break;
+        case "/":
+            divide(operand1, operand2);
+            break;
+        default:
+            break;
+    }
 }
